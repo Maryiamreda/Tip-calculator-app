4c52162ffb62c0afcb29f6c8550c1136ed621950
Maryiamreda <maryiamreda@gmail.com>
2025-02-07 16:15:38 +0200

    align items in center

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,11 @@ function App() {
   return (
 
     <div >
-      <Options />
-      <TipAmount />
+      <div className='flex'>
+        <Options />
+        <TipAmount />
+      </div>
+
     </div>
   )
 }
--- src/components/tip-amount.tsx
+++ src/components/tip-amount.tsx
@@ -3,14 +3,14 @@ const TipAmount = () => {
     return (
         <div className="bg-darkCyan">
             <div>
-                <div>
-                    <div>
+                <div className="flex">
+                    <div >
                         <h2>  Tip Amount   </h2>
                         <h3> / person</h3>
                     </div>
                     <h1>0.00</h1>
                 </div>
-                <div>
+                <div className="flex">
                     <div>
                         <h2>  Tip Amount   </h2>
                         <h3> / person</h3>
