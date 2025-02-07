288dcab09bb740141519ea5db00e8b3406b9c304
Maryiamreda <maryiamreda@gmail.com>
2025-02-08 00:00:44 +0200

    add results

--- src/components/tip-amount.tsx
+++ src/components/tip-amount.tsx
@@ -1,5 +1,5 @@
-import { useSelector } from 'react-redux';
-
+import { useSelector, useDispatch } from 'react-redux';
+import { setTipAmount, setTotal } from '../store/locationSlice';
 const TipAmount = () => {
     const { tipAmount, total } = useSelector((state: any) => state.tip);
 
@@ -11,17 +11,16 @@ const TipAmount = () => {
                         <h2>  Tip Amount   </h2>
                         <h3> / person</h3>
                     </div>
-                    <h1>0.00</h1>
-                    <h1>{tipAmount}</h1>
+                    {tipAmount == 0 ? (<h1>0.00</h1>) : (<h1>{tipAmount}</h1>
+                    )}
                 </div>
                 <div className="flex justify-between">
                     <div>
                         <h2>  Total   </h2>
                         <h3> / person</h3>
                     </div>
-                    <h1>0.00</h1>
-                    <h1>{total}</h1>
-
+                    {total == 0 ? (<h1>0.00</h1>) : (<h1>{total}</h1>
+                    )}
                 </div>
             </div>
             <button>RESET</button>
